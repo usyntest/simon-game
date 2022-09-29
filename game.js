6c3e155e69362fc99ddce381ab41b6546d1a99cd
uday <itsudayy@gmail.com>
2022-09-29 22:29:38 +0530

    basic functionaility added

--- game.js
+++ game.js
@@ -1,56 +1,89 @@
 var sequence = [];
-var level = 1;
+var colors = ["green", "red", "yellow", "blue"];
+var level = 0;
 var clicks = [];
 
-var btn = document.querySelectorAll(".btn");
+$("*").keydown(start);
 
-for (var i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", clicked);
+function start() {
+  console.log("it starts");
+
+  if ($("#level-title").hasClass("game-over")) {
+    $("#level-title").removeClass("game-over");
+  }
+  $("*").off("keydown", start);
+  changeLevel();
+  $(".btn").click(main);
 }
 
-function main() {
-  
-}
-
-function clicked() {
-  switch (this.id) {
-    case "red":
-      clicks.push("red");
-      check();
-      playAudio("red");
-      break;
-    case "yellow":
-      clicks.push("red");
-      check();
-      playAudio("yellow");
-      break;
-    case "green":
-      clicks.push("red");
-      check();
-      playAudio("green");
-      break;
-    default:
-      clicks.push("red");
-      check();
-      playAudio("blue");
-      break;
+function main(elem) {
+  // push the btn clicked into clicks
+  clicks.push($(elem).attr("id"));
+  // check if its eql to the element at the same place in sequence
+  var position = clicks.length - 1;
+  if (!(clicks[position] === sequence[position])) {
+    audioPlay("wrong");
+    $("#level-title").addClass("game-over");
+    $("#level-title").text("Press A Key to Start");
+  } else {
+    console.log("it is correct");
   }
 }
 
-function check() {
+function changeLevel() {
+  // increase the level and change the title
+  level += 1;
+  $("#level-title").text("Level " + level);
 
+  // add the color to the sequence
+  sequence.push(randomColor());
 }
 
-function playAudio(path) {
+function randomColor() {
+  // generatest a random number between 1 and 4 and gives out a color;
+  var random = Math.floor(Math.random() * 4) + 1;
+  var color = colors[random];
+
+  // the random color that is generated is highlighted
+  $(`#${color}`).addClass("pressed");
+  setTimeout(function () {
+    $(`#${color}`).removeClass("pressed");
+  }, 200);
+
+  // the color that is generated returned
+  return color;
+}
+
+function audioPlay(path) {
   var audio = new Audio(`sounds/${path}.mp3`);
   audio.play();
 }
+// function main() {
+//   while (true) {
+//     if (clicked(this)) {
+//       console.log("They are equal");
+//     } else {
+//       console.log("Pushed the wrong button");
+//       break;
+//     }
+//   }
+// }
 
-function nextSequence() {
-  var next = Math.floor(Math.random() * 4) + 1;
-  if (sequence[sequence.length - 1] === next) {
-    nextSequence();
-    return;
-  }
-  sequence.push(next);
-}
+// function start() {
+//   sequence.push(randomColor());
+//   $("#level-title");
+// }
+
+// function clicked(elem) {
+//   clicks.push($(elem).attr("id"));
+//   if (clicks === sequence) {
+//     changeLevel();
+//     return true;
+//   }
+//   return false;
+// }
+
+// function changeLevel() {
+//   level += 1;
+//   $("#levelTitle").text("Level " + level);
+// }
